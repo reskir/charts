62b7f66a657cba9dd59177cf36828043a025f217
Kiril Abaskin <kiril@1stdibs.com>
2019-10-28 21:46:46 +0200

    update index.js

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-
+const port = 5001;
 app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname + "/index.html"));
 });
 
 app.use("/data", express.static(path.join(__dirname + "/data")));
 
-app.listen(5001);
+app.listen(port, () =>
+    console.log(`App is running on: http://localhost:${port}`)
+);
